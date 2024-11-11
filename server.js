a414746f9685554d3462f38a424375f48e68bdbc
Tom <tomasz.asko@gmail.com>
2024-11-11 11:43:57 +0100

    server

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
- //const uri = `mongodb+srv://${process.env.DB_USER_ID}:${process.env.DB_PASSWORD}@cluster0.syiojsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-//const uri = process.env.DB_CONNECTION;
- const uri = `mongodb+srv://${process.env.DB_USER_ID}:${process.env.DB_PASSWORD}@cluster0.syiojsr.mongodb.net/Node-API-collection?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = process.env.DB_CONNECTION;
 const FRONTEND = process.env.FRONTEND;
 
 const Product = require('./models/productModel');
